4975bdf7b3d899213a15ab23537c57fe16be3b11
ParthCodeBucket <parthkodapepsv@gmail.com>
2020-07-08 16:14:29 +0530

    Cards having name of my robot friends and their robot avatars displayed according to their names.

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Aryan Jain',
+    username: 'Aryan',
+    email: 'AryanJain@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Raman Baraskar',
+    username: 'Raman',
+    email: 'RamanBaraskar@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Ashutosh Mahore',
+    username: 'Ashu',
+    email: 'AshutoshMahore@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Prakash Mishra',
+    username: 'Prakash',
+    email: 'PrakashMishra@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Sarvesh Kulkarni',
+    username: 'Sarvesh',
+    email: 'SarveshKulkarni@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Nihal Arkhel',
+    username: 'Nihal',
+    email: 'NihalArkhel@gmail.com'
+  }
+]
